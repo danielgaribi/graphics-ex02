459340ace3576be6fe814af40d090698b78bcd8f
Tlusty <tlustyofer@gmail.com>
2023-06-04 17:51:17 +0300

    Start implementing computing color of a pixel and basic structure

--- ray_tracer.py
+++ ray_tracer.py
@@ -10,6 +10,8 @@ from surfaces.cube import Cube
 from surfaces.infinite_plane import InfinitePlane
 from surfaces.sphere import Sphere
 
+OBJECT_IDX  = 0
+DIST_IDX    = 1
 
 def parse_scene_file(file_path):
     objects = []
@@ -46,13 +48,101 @@ def parse_scene_file(file_path):
                 raise ValueError("Unknown object type: {}".format(obj_type))
     return camera, scene_settings, objects
 
-
 def save_image(image_array):
     image = Image.fromarray(np.uint8(image_array))
 
     # Save the image to a file
     image.save("scenes/Spheres.png")
 
+def construct_ray_through_pixel(): 
+    pass 
+
+# TODO: Garibi - return a tuple of (surface, dist) of all objects intersacting the ray as a sorted array by distance (first is the closest to the origin of the ray)
+def find_intersection(object_array, ray):
+    pass
+
+def find_cordination(ray, dist):
+    pass 
+
+# return the normal of the surface - normalized
+def compute_surface_normal(surface_obj, surface_dist):
+    pass 
+
+def compute_intencity():
+    pass
+
+def compute_reflection_ray(vec1, normal):
+    teta = np.dot(vec1, normal)
+    vec2 = vec1 - 2 * teta * normal
+    vec2 = vec2 / np.linalg.norm(vec2)
+    return vec2
+
+def compute_diffuse_color(light, light_intensity, intersection_cord, normal):
+    L_vec = light.position - intersection_cord
+    L_vec = L_vec / np.linalg.norm(L_vec)
+    N_dot_L = np.dot(normal, L_vec)
+    
+    if (N_dot_L < 0 ):
+        return np.zeros(3, dtype=float)
+    
+    return light.color * N_dot_L * light_intensity
+
+def compute_specular_color(light, light_intensity, cam_pos, intersection_cord, normal, shininess):
+    L_vec = light.position - intersection_cord
+    L_vec = L_vec / np.linalg.norm(L_vec)
+
+    R_vec = compute_reflection_ray(L_vec, normal)
+    
+    V_vec = cam_pos - intersection_cord
+    V_vec = V_vec / np.linalg.norm(V_vec)
+
+    V_dot_R = np.dot(V_vec, R_vec)
+    if (V_dot_R < 0):
+        return np.zeros(3, dtype=float)
+    
+    return light.color * np.power(V_dot_R, shininess) * light_intensity * light.specular_intensity
+    
+def copmute_surface_color(scene_settings, ray, cam_pos, surfaces, surface_idx, material_array, light_array, recursion_level):
+    curr_surface_obj    = surfaces[surface_idx][OBJECT_IDX]
+    curr_surface_dist   = surfaces[surface_idx][DIST_IDX]
+    
+    intersection_cord = find_cordination(ray, curr_surface_dist)
+    normal = compute_surface_normal(curr_surface_obj, curr_surface_dist)
+
+    # Get surface's material
+    curr_material = material_array[curr_surface_obj.material_index]
+
+    bg_color         = np.array(scene_settings.background_color)
+    diffuse_color    = np.zeros(3, dtype=float)
+    specular_color   = np.zeros(3, dtype=float)
+    reflection_color = np.zeros(3, dtype=float)
+
+    for light in light_array:
+        light_intensity = compute_intencity()
+        diffuse_color += compute_diffuse_color(light, light_intensity, intersection_cord, normal)
+        specular_color += compute_specular_color(light, light_intensity, cam_pos, intersection_cord, normal, curr_material.shininess)
+
+    if (recursion_level < scene_settings.max_recursions):
+        reflaction_ray = compute_reflection_ray(ray, normal)
+        reflection_color = compute_pixel_color(scene_settings, reflaction_ray, surfaces, material_array, light_array, cam_pos, recursion_level + 1)
+
+    if ((curr_material.transparency > 0.0) and (surface_idx + 1 < len(surfaces))):
+        bg_color *= copmute_surface_color(scene_settings, ray, cam_pos, surfaces, surface_idx + 1, material_array, light_array, 0)
+
+    diffuse_color    *= curr_material.diffuse_color
+    specular_color   *= curr_material.specular_color
+    reflection_color *= curr_material.reflection_color
+
+    return bg_color * curr_material.transparency + (diffuse_color + specular_color) * (1 - curr_material.transparency) + reflection_color
+
+def compute_pixel_color(scene_settings, ray, object_array, material_array, light_array, cam_pos, recursion_level):
+    surfaces = find_intersection(object_array, ray)
+    if (len(surfaces) == 0):
+        output_color = scene_settings.background_color
+    else:
+        output_color = copmute_surface_color(scene_settings, ray, cam_pos, surfaces, 0, material_array, light_array, recursion_level)
+    
+    return np.array(output_color)
 
 def main():
     parser = argparse.ArgumentParser(description='Python Ray Tracer')
@@ -65,14 +155,30 @@ def main():
     # Parse the scene file
     camera, scene_settings, objects = parse_scene_file(args.scene_file)
 
-    # TODO: Implement the ray tracer
+    # Divide objects into materials, lights, and other objects
+    material_array = []
+    light_array = []
+    object_array = []
+    for obj in objects:
+        if isinstance(obj, Material):
+            material_array.append(obj)
+        elif isinstance(obj, Light):
+            light_array.append(obj)
+        else:
+            object_array.append(obj)
 
-    # Dummy result
-    image_array = np.zeros((500, 500, 3))
+    # TODO - remove - basic init
+    img_width = 500
+    img_height = 500
+
+    image_array = np.zeros((img_height, img_width, 3), dtype=float)
+    for i in range(img_width):
+        for j in range(img_height):
+            ray = construct_ray_through_pixel()
+            image_array[j, i] = compute_pixel_color(scene_settings, ray, object_array, material_array, light_array, camera.position, 0)
 
     # Save the output image
     save_image(image_array)
-
 
 if __name__ == '__main__':
     main()
